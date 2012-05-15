2c96378ed4596b2c332c51cd14d851d88b7491bf
nischayn22 <nischayn22@gmail.com>
2012-05-15 10:08:26 +0530

    Added i18n from translatewiki.net

--- AllTimeZones.i18n.php
+++ AllTimeZones.i18n.php
@@ -13,5 +13,146 @@ $messages = array();
  * @author Nischay Nahata
  */
 $messages['en'] = array(
-	'alltimezones' => 'This is a tag extension and lets you show dates in all time zones',
+	'alltimezones-desc' => 'Adds a tag to show dates in all time zones',
 );
+
+/** Afrikaans (Afrikaans)
+ * @author Naudefj
+ */
+$messages['af'] = array(
+	'alltimezones-desc' => "Maak 'n etiket beskikbaar om datums in alle tydsones te wys",
+);
+
+/** Arabic (العربية)
+ * @author DRIHEM
+ */
+$messages['ar'] = array(
+	'alltimezones-desc' => 'يضيف وسما لعرض التواريخ في كافة المناطق الزمنية',
+);
+
+/** Asturian (Asturianu)
+ * @author Xuacu
+ */
+$messages['ast'] = array(
+	'alltimezones-desc' => "Amiesta una etiqueta p'amosar les dates en toles estayes horaries",
+);
+
+/** Belarusian (Taraškievica orthography) (‪Беларуская (тарашкевіца)‬)
+ * @author Wizardist
+ */
+$messages['be-tarask'] = array(
+	'alltimezones-desc' => 'Дадае тэг для адлюстраваньня датаў ва ўсіх часавых пасах',
+);
+
+/** Breton (Brezhoneg)
+ * @author Fulup
+ */
+$messages['br'] = array(
+	'alltimezones-desc' => 'Ouzhpennañ a ra un dikedenn evit diskouez an deiziadoù en holl werzhidoù eur.',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'alltimezones-desc' => 'Ergänzt das Tag <code><timezone></code> mit dem man eine zeitzonenabhängige Zeitangabe einbinden kann',
+);
+
+/** Lower Sorbian (Dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'alltimezones-desc' => 'Pśidawa toflicku, aby datumy we wšych casowych pasmach pokazało',
+);
+
+/** Esperanto (Esperanto)
+ * @author ArnoLagrange
+ */
+$messages['eo'] = array(
+	'alltimezones-desc' => 'Aldonas markilon por vidigi datojn en ĉiuj horzonoj.',
+);
+
+/** Spanish (Español)
+ * @author Armando-Martin
+ */
+$messages['es'] = array(
+	'alltimezones-desc' => 'Añadir una etiqueta para mostrar las fechas en todas las zonas horarias',
+);
+
+/** French (Français)
+ * @author Wyz
+ */
+$messages['fr'] = array(
+	'alltimezones-desc' => 'Ajoute une balise pour afficher les dates dans tous les fuseaux horaires',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'alltimezones-desc' => 'Engade unha etiqueta para mostrar as datas en todos os fusos horarios',
+);
+
+/** Hebrew (עברית)
+ * @author Amire80
+ */
+$messages['he'] = array(
+	'alltimezones-desc' => 'הוספת תג שמציג תאריכים בכל אזורי הזמן',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'alltimezones-desc' => 'Přidawa tafličku, zo by datumy we wšěch časowych pasmach pokazało',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'alltimezones-desc' => 'Adde un etiquetta pro monstrar datas in tote le fusos horari',
+);
+
+/** Korean (한국어)
+ * @author Kwj2772
+ */
+$messages['ko'] = array(
+	'alltimezones-desc' => '모든 시간대의 날짜를 표시하는 태그를 추가',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'alltimezones-desc' => 'Setzt en Tag derbäi fir Datumen an allen Zäitzonen ze weisen',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'alltimezones-desc' => 'Додава ознака за прикажување на датумите во сите часовни појаси',
+);
+
+/** Portuguese (Português)
+ * @author Malafaya
+ */
+$messages['pt'] = array(
+	'alltimezones-desc' => 'Adiciona uma marca para mostrar datas em todos os fusos horários',
+);
+
+/** Russian (Русский)
+ * @author Eleferen
+ */
+$messages['ru'] = array(
+	'alltimezones-desc' => 'Добавляет тег для отображения дат во всех часовых поясах',
+);
+
+/** Swedish (Svenska)
+ * @author Lokal Profil
+ */
+$messages['sv'] = array(
+	'alltimezones-desc' => 'Lägger till en tagg för att visa datum i alla tidszoner',
+);
+
